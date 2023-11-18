7fa8b7d0bc7235b8a5f5de0e56b5b3e98a57a244
MehmetTurkerofRivia <mertmemo69@gmail.com>
2023-11-18 13:58:19 +0300

    Ziplama bitti

--- MagaraJam/Assets/Scripts/Tags/TagManager.cs
+++ MagaraJam/Assets/Scripts/Tags/TagManager.cs
@@ -29,6 +29,7 @@ public class TagManager
     public static string DEAD_PARAMETER = "Dead";
     public static string RANDOM_PARAMETER = "Random";
     public static string ENEMY_MOVE_ANIMATION = "Move";
+    public static string JUMP_ANIMATION = "Jump";
 
     // LEVEL NAMES
     public static string LEVEL_1_NAME = "Gameplay 1";
