f7497ecac67db5a0c0d25bf0532c3271de4796e5
Mehmet Ali <mehmetalid967@gmail.com>
2023-11-19 15:59:51 +0300

    yetenek secme panelı

---
+++ MagaraJam/Assets/AbilityPanel.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityPanel : MonoBehaviour
+{
+    [SerializeField] GameObject LevelUpPanel;
+    [SerializeField] Transform[] augment_positions;
+    [SerializeField] GameObject[] Augments;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            openPanel();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape)){
+            ClosePanel();
+        }
+    }
+
+    public void openPanel()
+    {
+        Time.timeScale = 0;
+       
+        LevelUpPanel.SetActive(true);
+        idenitfyAugments();
+    }
+    public void ClosePanel()
+    {
+        Time.timeScale = 1;
+        LevelUpPanel.SetActive(false);
+    }
+    void idenitfyAugments()
+    {
+        int a0 = Random.Range(0, Augments.Length);
+        int a1 = Random.Range(0, Augments.Length);
+       
+        GameObject augment0 = Instantiate(Augments[a0], augment_positions[0]);
+        GameObject augment1 = Instantiate(Augments[a1], augment_positions[1]);
+        
+    }
+}
